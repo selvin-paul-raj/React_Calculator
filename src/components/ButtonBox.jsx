90de75bf8199b5efe85c169786c40cca32f4e228
sprhackz <sprhackz003@gmail.com>
2023-11-15 10:26:51 +0530

    React Calculator

---
+++ src/components/ButtonBox.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ButtonBox = ({children}) => {
+  return (
+    <div className='buttonBox'>{children}</div>
+  )
+}
+
+export default ButtonBox
---
+++ src/components/Wrapper.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Wrapper = ({children}) => {
+  return (
+    <div className='wrapper'>{children}</div>
+  )
+}
+
+export default Wrapper
